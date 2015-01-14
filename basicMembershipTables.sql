f79f5f8a45d5610d30eda3049f139574b6a4b70b
Kevin Viratyosin <virat001@umn.edu>
2015-01-14 10:10:05 -0600

    Added reward table

--- basicMembershipTables.sql
+++ basicMembershipTables.sql
@@ -110,6 +110,26 @@ CREATE TABLE waiver_status (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table reward
+--
+DROP TABLE IF EXISTS reward;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE reward (
+  id int(10) NOT NULL AUTO_INCREMENT,
+  member_id smallint(10) NOT NULL,
+  kind varchar(255) NOT NULL,
+  issue_date_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  claim_date_time timestamp NULL,
+  claimed tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (id),
+  INDEX (member_id),
+  FOREIGN KEY (member_id) 
+    REFERENCES member(id)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table referral
 --
 DROP TABLE IF EXISTS referral;
