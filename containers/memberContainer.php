91413a540cf336af16b0cf8a3348de402a513bd6
Kevin Viratyosin <virat001@umn.edu>
2015-01-16 17:07:58 -0600

    Put member info tables in collapsible panels

--- containers/memberContainer.php
+++ containers/memberContainer.php
@@ -30,56 +30,103 @@
     </div>
   </p>
   
-  <h3>Credits and debits</h3>
-  <table id="memberCreditDebitTable" class="table table-condensed">
-    <thead>
-      <tr>
-        <th>Credit/Debit</th>
-        <th>Amount</th>
-        <th>Kind</th>
-        <th>Method</th>
-        <th>Charged/Paid</th>
-      </tr>
-    </thead>
-    <tbody>
-    </tbody>
-  </table>
-  
-  <h3>Rewards</h3>
-  <table id="memberRewardsTable" class="table table-condensed table-hover">
-    <thead>
-      <tr>
-        <th>Kind</th>
-        <th>Term</th>
-        <th>Issue Date</th>
-        <th>Claimed</th>
-      </tr>
-    </thead>
-    <tbody>
-    </tbody>
-  </table>
-  
-  <h3>Check In History</h3>
-  <table id="memberHistoryTable" class="table table-condensed">
-    <thead>
-      <tr>
-        <th>When</th>
-      </tr>
-    </thead>
-    <tbody>
-    </tbody>
-  </table>
-  
-  <h3>References</h3>
-  <p id="referredByP">Referred by: <span id="referredBySpan"></span></p>
-  <table id="referredTable" class="table table-condensed table-hover">
-    <thead>
-      <tr>
-        <th>Referred</th>
-      </tr>
-    </thead>
-    <tbody>
-    </tbody>
-  </table>
+  <div class="panel-group" id="memberPanels" role="tablist" aria-multiselectable="true">
+    <div class="panel panel-default">
+      <div class="panel-heading" role="tab" id="creditsAndDebitsPanelHeading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#memberPanels" href="#creditsAndDebitsPanel" aria-expanded="true" aria-controls="creditsAndDebitsPanel">
+            Credits and debits
+          </a>
+        </h4>
+      </div>
+      <div id="creditsAndDebitsPanel" class="panel-collapse collapse" role="tabpanel" aria-labelledby="creditsAndDebitsPanelHeading">
+        <div class="panel-body">
+          <table id="memberCreditDebitTable" class="table table-condensed">
+            <thead>
+              <tr>
+                <th>Credit/Debit</th>
+                <th>Amount</th>
+                <th>Kind</th>
+                <th>Method</th>
+                <th>Charged/Paid</th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading" role="tab" id="rewardsPanelHeading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#memberPanels" href="#rewardsPanel" aria-expanded="true" aria-controls="rewardsPanel">
+            Rewards
+          </a>
+        </h4>
+      </div>
+      <div id="rewardsPanel" class="panel-collapse collapse" role="tabpanel" aria-labelledby="rewardsPanelHeading">
+        <div class="panel-body">
+          <table id="memberRewardsTable" class="table table-condensed table-hover">
+            <thead>
+              <tr>
+                <th>Kind</th>
+                <th>Term</th>
+                <th>Issue Date</th>
+                <th>Claimed</th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading" role="tab" id="checkinHistoryPanelHeading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#memberPanels" href="#checkinHistoryPanel" aria-expanded="true" aria-controls="checkinHistoryPanel">
+            Check in history
+          </a>
+        </h4>
+      </div>
+      <div id="checkinHistoryPanel" class="panel-collapse collapse" role="tabpanel" aria-labelledby="checkinHistoryPanelHeading">
+        <div class="panel-body">
+          <table id="memberHistoryTable" class="table table-condensed">
+            <thead>
+              <tr>
+                <th>When</th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading" role="tab" id="referencesPanelHeading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#memberPanels" href="#referencesPanel" aria-expanded="true" aria-controls="referencesPanel">
+            References
+          </a>
+        </h4>
+      </div>
+      <div id="referencesPanel" class="panel-collapse collapse" role="tabpanel" aria-labelledby="referencesPanelHeading">
+        <div class="panel-body">
+          <p id="referredByP">Referred by: <span id="referredBySpan"></span></p>
+          <table id="referredTable" class="table table-condensed table-hover">
+            <thead>
+              <tr>
+                <th>Referred</th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
   
 </div>
