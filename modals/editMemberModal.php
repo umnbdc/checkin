d84db27ae21ef414afccffe928b71b7d1aca48a4
Kyle Condiff <kyle@x-10-104-132-213.uofm-secure.wireless.umn.edu>
2016-09-13 19:41:17 -0500

    revert advanced lesson checkins

--- modals/editMemberModal.php
+++ modals/editMemberModal.php
@@ -27,7 +27,6 @@
           <select class="form-control" id="inputEditProficiency">
             <option value="Beginner">Beginner</option>
             <option value="Intermediate">Intermediate</option>
-            <option value="Advanced">Advanced</option>
           </select>
         </div>
       </div>
